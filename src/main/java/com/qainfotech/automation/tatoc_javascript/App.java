bd9c088db046ec064ade60a3c0e9dbff868a435e
bhavyaqa <40259301+bhavyaqa@users.noreply.github.com>
2018-06-27 12:36:45 +0530

    "all tests success"

--- src/main/java/com/qainfotech/automation/tatoc_javascript/App.java
+++ src/main/java/com/qainfotech/automation/tatoc_javascript/App.java
@@ -1,6 +1,7 @@
 package com.qainfotech.automation.tatoc_javascript;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
@@ -14,80 +15,81 @@ public class App
 {
 	WebDriver driver;
 	JavascriptExecutor js;
-	
-	public App(WebDriver driver,JavascriptExecutor js) {
-		this.driver = driver;
-		this.js = js;
+	public App() {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\bhavyakhanna\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		
+		this.driver = new ChromeDriver();
+		this.js = (JavascriptExecutor) this.driver;
 	}
-
+	
 	public void launchBrowser() {
 		
 		driver.get("http://10.0.1.86/tatoc");
 	}
 
 	public void openBasic() {
-
-		driver.findElement(By.tagName("a")).click();
-		js.executeScript("document.getElementsByTagname('a').click()");
+		js.executeScript("document.getElementsByTagName('a')[0].click()");
 	}
 
-//	public void gridGate() {
-//		driver.findElement(By.className("greenbox")).click();
-//	}
-//
-//	public void frameDungeon() {
-//		driver.switchTo().frame(driver.findElement(By.id("main")));
-//		WebElement box1 = driver.findElement(By.id("answer"));
-//		String color1 = box1.getAttribute("class");
-//		driver.switchTo().frame(driver.findElement(By.id("child")));
-//		WebElement box2 = driver.findElement(By.id("answer"));
-//		String color2 = box2.getAttribute("class");
-//		while (!color1.equals(color2)) {
-//			driver.switchTo().defaultContent();
-//			driver.switchTo().frame(driver.findElement(By.id("main")));
-//			driver.findElement(By.tagName("a")).click();
-//			driver.switchTo().frame(driver.findElement(By.id("child")));
-//			box2 = driver.findElement(By.id("answer"));
-//			color2 = box2.getAttribute("class");
-//		}
-//		driver.switchTo().defaultContent();
-//		driver.switchTo().frame(driver.findElement(By.id("main")));
-//		List<WebElement> anchors = driver.findElements(By.tagName("a"));
-//		anchors.get(anchors.size() - 1).click();
-//	}
-//
-//	public void dragAround() {
-//		WebElement from = driver.findElement(By.id("dragbox"));
-//		WebElement to = driver.findElement(By.id("dropbox"));
-//		Actions act = new Actions(driver);
-//		act.dragAndDrop(from, to).build().perform();
-//		driver.findElement(By.tagName("a")).click();
-//	}
-//
-//	public void popupWindow() {
-//		List<WebElement> popup = driver.findElements(By.tagName("a"));
-//		popup.get(0).click();
-//		String originalwindow = driver.getWindowHandle();
-//		for (String handle : driver.getWindowHandles()) {
-//			driver.switchTo().window(handle);
-//
-//		}
-//		driver.findElement(By.id("name")).sendKeys("Bhavya");
-//		driver.findElement(By.id("submit")).click();
-//		driver.switchTo().window(originalwindow);
-//		popup.get(1).click();
-//	}
-//
-//	public void cookieHandling() {
-//		List<WebElement> CookieHandlingLinks = driver.findElements(By.tagName("a"));
-//		CookieHandlingLinks.get(0).click();
-//		String token = driver.findElement(By.id("token")).getText();
-//		String newtoken = token.replace("Token: ", "");
-//		Cookie tatoc_cookie = new Cookie("Token", newtoken);
-//		driver.manage().addCookie(tatoc_cookie);
-//		CookieHandlingLinks.get(1).click();
-//	}
+	public void gridGate() {
+		js.executeScript("document.getElementsByClassName('greenbox')[0].click()");
+		
+	}
+
+	public void frameDungeon() {
+		WebElement mainframe = (WebElement)js.executeScript("return document.getElementById('main')");
+		driver.switchTo().frame(mainframe);
+		WebElement box1 = (WebElement) js.executeScript("return document.getElementById('answer')");
+		String color1 = box1.getAttribute("class");
+		WebElement childframe = (WebElement) js.executeScript("return document.getElementById('child')");
+		driver.switchTo().frame(childframe);
+		WebElement box2 = (WebElement) js.executeScript("return document.getElementById('answer')");
+		String color2 = box2.getAttribute("class");
+		while (!color1.equals(color2)) {
+			driver.switchTo().defaultContent();
+			driver.switchTo().frame(mainframe);
+			js.executeScript("document.getElementsByTagName('a')[0].click()");
+			driver.switchTo().frame(childframe);
+			box2 = (WebElement) js.executeScript("return document.getElementById('answer')");;
+			color2 = box2.getAttribute("class");
+		}
+		driver.switchTo().defaultContent();
+		driver.switchTo().frame(mainframe);
+		List<WebElement> anchors = 	(List<WebElement>) js.executeScript("return document.getElementsByTagName('a')");
+
+		anchors.get(anchors.size() - 1).click();
+	}
+
+	public void dragAround() {
+		
+		WebElement from = (WebElement) js.executeScript("return document.getElementById('dragbox')");
+		WebElement to = (WebElement) js.executeScript("return document.getElementById('dropbox')");
+		Actions act = new Actions(driver);
+		act.dragAndDrop(from, to).build().perform();
+		js.executeScript("document.getElementsByTagName('a')[0].click()");
+	}
+
+	public void popupWindow() {
+		List<WebElement> popup = (List<WebElement>) js.executeScript("return document.getElementsByTagName('a')");
+		popup.get(0).click();
+		String originalwindow = driver.getWindowHandle();
+		for (String handle : driver.getWindowHandles()) {
+			driver.switchTo().window(handle);
+
+		}
+		js.executeScript("document.getElementById('name').setAttribute('value','Bhavya')");
+		js.executeScript("document.getElementById('submit').click()");
+		driver.switchTo().window(originalwindow);
+		popup.get(1).click();
+	}
+
+	public void cookieHandling() {
+		List<WebElement> CookieHandlingLinks = (List<WebElement>) js.executeScript("return document.getElementsByTagName('a')");
+		CookieHandlingLinks.get(0).click();
+		String token = (String)js.executeScript("return document.getElementById('token').innerText");
+		String newtoken = token.replace("Token: ", "");
+		Cookie tatoc_cookie = new Cookie("Token", newtoken);
+		driver.manage().addCookie(tatoc_cookie);
+		CookieHandlingLinks.get(1).click();
+	}
 }
--- src/test/java/com/qainfotech/automation/tatoc_javascript/AppTest.java
+++ src/test/java/com/qainfotech/automation/tatoc_javascript/AppTest.java
@@ -1,16 +1,30 @@
 package com.qainfotech.automation.tatoc_javascript;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-public class AppTest 
+public class AppTest extends App
 {
    @BeforeClass
    public void launch_browser() {
-	   
+	   super.launchBrowser();   
    }
-    
+   @Test
+   public void all_tatoc() {
+	   super.openBasic();
+	   super.gridGate();
+	   super.frameDungeon();
+	   super.dragAround();
+	   super.popupWindow();
+	   super.cookieHandling();
+   }
+   
+
+   @AfterClass
+   public void close_browser() {
+	   driver.quit();
+   }
 }
